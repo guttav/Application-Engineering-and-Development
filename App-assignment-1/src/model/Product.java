34cfaa6c6ec34ebd622258ef5b67b8237e277e93
guttav <gutta.v@northeastern.edu>
2022-09-23 20:55:09 -0400

    assignment init submission

---
+++ App-assignment-1/src/model/Product.java
@@ -0,0 +1,203 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hp
+ */
+public class Product {
+    
+    private String usr_fname;
+    private String usr_lname;
+    private String usr_dob;
+    private String usr_age;
+    private String usr_tel_phn_no;
+    private String usr_email;
+    private String usr_street_line_1;
+    private String usr_street_line_2;
+    private String usr_city;
+    private String usr_country;
+    private String usr_affiliation;
+    private String usr_major;
+    private String usr_career;
+    private String usr_degree1;
+    private String usr_degree1_start;
+    private String usr_degree1_end;
+    private String usr_degree2;
+    private String usr_degree2_start;
+    private String usr_degree2_end;
+    private String usr_;
+    private String usr_photo_label;
+
+    public String getUsr_photo_label() {
+        return usr_photo_label;
+    }
+
+    public void setUsr_photo_label(String usr_photo_label) {
+        this.usr_photo_label = usr_photo_label;
+    }
+
+    public String getUsr_fname() {
+        return usr_fname;
+    }
+
+    public void setUsr_fname(String usr_fname) {
+        this.usr_fname = usr_fname;
+    }
+
+    public String getUsr_lname() {
+        return usr_lname;
+    }
+
+    public void setUsr_lname(String usr_lname) {
+        this.usr_lname = usr_lname;
+    }
+
+    public String getUsr_dob() {
+        return usr_dob;
+    }
+
+    public void setUsr_dob(String usr_dob) {
+        this.usr_dob = usr_dob;
+    }
+
+    public String getUsr_age() {
+        return usr_age;
+    }
+
+    public void setUsr_age(String usr_age) {
+        this.usr_age = usr_age;
+    }
+
+    public String getUsr_tel_phn_no() {
+        return usr_tel_phn_no;
+    }
+
+    public void setUsr_tel_phn_no(String usr_tel_phn_no) {
+        this.usr_tel_phn_no = usr_tel_phn_no;
+    }
+
+    public String getUsr_email() {
+        return usr_email;
+    }
+
+    public void setUsr_email(String usr_email) {
+        this.usr_email = usr_email;
+    }
+
+    public String getUsr_street_line_1() {
+        return usr_street_line_1;
+    }
+
+    public void setUsr_street_line_1(String usr_street_line_1) {
+        this.usr_street_line_1 = usr_street_line_1;
+    }
+
+    public String getUsr_street_line_2() {
+        return usr_street_line_2;
+    }
+
+    public void setUsr_street_line_2(String usr_street_line_2) {
+        this.usr_street_line_2 = usr_street_line_2;
+    }
+
+    public String getUsr_city() {
+        return usr_city;
+    }
+
+    public void setUsr_city(String usr_city) {
+        this.usr_city = usr_city;
+    }
+
+    public String getUsr_country() {
+        return usr_country;
+    }
+
+    public void setUsr_country(String usr_country) {
+        this.usr_country = usr_country;
+    }
+
+    public String getUsr_affiliation() {
+        return usr_affiliation;
+    }
+
+    public void setUsr_affiliation(String usr_affiliation) {
+        this.usr_affiliation = usr_affiliation;
+    }
+
+    public String getUsr_major() {
+        return usr_major;
+    }
+
+    public void setUsr_major(String usr_major) {
+        this.usr_major = usr_major;
+    }
+
+    public String getUsr_career() {
+        return usr_career;
+    }
+
+    public void setUsr_career(String usr_career) {
+        this.usr_career = usr_career;
+    }
+
+    public String getUsr_degree1() {
+        return usr_degree1;
+    }
+
+    public void setUsr_degree1(String usr_degree1) {
+        this.usr_degree1 = usr_degree1;
+    }
+
+    public String getUsr_degree1_start() {
+        return usr_degree1_start;
+    }
+
+    public void setUsr_degree1_start(String usr_degree1_start) {
+        this.usr_degree1_start = usr_degree1_start;
+    }
+
+    public String getUsr_degree1_end() {
+        return usr_degree1_end;
+    }
+
+    public void setUsr_degree1_end(String usr_degree1_end) {
+        this.usr_degree1_end = usr_degree1_end;
+    }
+
+    public String getUsr_degree2() {
+        return usr_degree2;
+    }
+
+    public void setUsr_degree2(String usr_degree2) {
+        this.usr_degree2 = usr_degree2;
+    }
+
+    public String getUsr_degree2_start() {
+        return usr_degree2_start;
+    }
+
+    public void setUsr_degree2_start(String usr_degree2_start) {
+        this.usr_degree2_start = usr_degree2_start;
+    }
+
+    public String getUsr_degree2_end() {
+        return usr_degree2_end;
+    }
+
+    public void setUsr_degree2_end(String usr_degree2_end) {
+        this.usr_degree2_end = usr_degree2_end;
+    }
+
+    public String getUsr_() {
+        return usr_;
+    }
+
+    public void setUsr_(String usr_) {
+        this.usr_ = usr_;
+    }
+    
+}
